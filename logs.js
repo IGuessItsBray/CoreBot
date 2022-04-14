e761f8da3222a007b2bb1fbd932f251c254b7143
IGuessItsBray <97918955+IGuessItsBray@users.noreply.github.com>
2022-04-14 12:45:22 -0400

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,13 +1,21 @@
 const Auditlog = require("discord-auditlog");
 module.exports = (client) => {
     Auditlog(client, {
-        "955230769939353623": {
-            auditlog: "audit-log-testing",
-            movement: "audit-log-testing",
-            auditmsg: "audit-log-testing", // Default to fasle, recommend to set a channel
-            voice: "audit-log-testing", // Set a Channel name if you want it
+        "804915620654350346": {
+            auditlog: "audit-log",
+            movement: "audit-log",
+            auditmsg: "audit-log", // Default to fasle, recommend to set a channel
+            voice: "audit-log", // Set a Channel name if you want it
             trackroles: true, // Default is False
-            excludedroles: ['', '']  // This is an OPTIONAL array of Roles ID that won't be tracked
+            excludedroles: ['950969966918766625', '']  // This is an OPTIONAL array of Roles ID that won't be tracked
+        },
+        "945205088287326248": {
+            auditlog: "audit-log",
+            movement: "audit-log",
+            auditmsg: "audit-log", // Default to fasle, recommend to set a channel
+            voice: "audit-log", // Set a Channel name if you want it
+            trackroles: true, // Default is False
+            excludedroles: ['947305031466627095', '']  // This is an OPTIONAL array of Roles ID that won't be tracked
         }
     });
 };
