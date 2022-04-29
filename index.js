b6e8d11621c526d5a89d961b577fac4bfa9438d7
IGuessItsBray <97918955+IGuessItsBray@users.noreply.github.com>
2022-04-29 07:24:10 -0400

    V2 Update
    Co-Authored-By: Seth Van Niekerk <seth@swvn.io>
    Co-Authored-By: PMass <22862636+pmass@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,97 +1,40 @@
 // ------------------------------------------------------------------------------
-// Getting Ready
+// index.js
+// The actual running of the bot.
 // ------------------------------------------------------------------------------
 
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
-const config = require("./config.json");
-const { Player } = require('discord-player')
+const config = require('./config.json');
+const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
-const { readdirSync } = require('fs');
 
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILD_MEMBERS,
+const client =
+	new Client({
+		intents: [
+			Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_BANS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ]
-});
+        Intents.FLAGS.GUILD_VOICE_STATES,
+		],
+		partials: [
+			'MESSAGE',
+		],
+	});
 
-//Load Commands from /commands/*folder*
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const commandFolders = fs.readdirSync('./commands');
-
-for (const folder of commandFolders) {
-    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const command = require(`./commands/${folder}/${file}`);
-        client.commands.set(command.name, command);
-    }
-}
-
-//Initialize the DB
-require('./db/mongo').initMongo();
+module.exports = { client };
 
 // ------------------------------------------------------------------------------
-// STARTUP / READY Event & Command Registration
+// Initializing Bot Components
 // ------------------------------------------------------------------------------
 
-client.once('ready', () => {
-    // registerCommands();
-    console.info(`✅ Bot │ Ready & Running as ${client.user.tag}`);
-    client.user.setPresence({
-        activities: [
-            {
-                type: 'WATCHING',
-                name: `${client.guilds.cache.size} Discord Servers`
-            },
-        ],
-        status: 'online'
-    });
-});
-
-client.on('guildCreate', guild => {
-    console.info(`Joined ${guild.name}.`);
-    registerCommands(guild.id);
-});
-
-// ------------------------------------------------------------------------------
-// Listening / Event Handling
-// ------------------------------------------------------------------------------
-
-// Command consumption and handling
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand() && !interaction.isContextMenu()) return;
-    const guild = interaction.guild;
-    const user = interaction.user;
-    const subcommand = interaction.options._subcommand ?? ``;
-    try {
-        //console.log(`${guild.id}: ${user.tag}: Executed ${interaction.commandName} ${subcommand}`);
-        await client
-            .commands.get(interaction.commandName)
-            .execute(interaction);
-    }
-    catch (error) {
-        console.error(error);
-        await interaction.reply({
-            content: 'Something went wrong executing this interaction.',
-            ephemeral: true,
-        }).catch(() => {
-            console.error(`${guild.id}: ${user.tag}: Something went wrong executing this interaction.`)
-        });
-    }
-});
-
-
-// ------------------------------------------------------------------------------
-// Bot Login
-// ------------------------------------------------------------------------------
+require('./db/mongo').init();
+require('./init/initCommands').init(client);
 
 client.login(token);
+
+client.once('ready', () => {
+	console.info(`Ready & Running as ${client.user.tag}`);
+});
 
 // ------------------------------------------------------------------------------
 // Addons
