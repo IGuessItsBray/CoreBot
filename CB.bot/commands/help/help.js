96340266c112df47114d35a5719bcee72669d7b7
IGuessItsBray <braydendelaire5@gmail.com>
2024-09-03 11:59:38 -0400

    Update the help command for accurate devs

--- CB.bot/commands/help/help.js
+++ CB.bot/commands/help/help.js
@@ -1,76 +1,124 @@
-const { Client, Intents, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ApplicationCommandType, ApplicationCommandOptionType } = require('discord.js');
-const { PermissionFlagsBits, ButtonStyle } = require('discord.js');
-const { COMMAND, OPTION, CHANNEL } = require('../../util/enum').Types;
+const {
+  Client,
+  Intents,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ApplicationCommandType,
+  ApplicationCommandOptionType,
+} = require("discord.js");
+const { PermissionFlagsBits, ButtonStyle } = require("discord.js");
+const { COMMAND, OPTION, CHANNEL } = require("../../util/enum").Types;
 module.exports = {
+  // ------------------------------------------------------------------------------
+  // Definition
+  // ------------------------------------------------------------------------------
 
-    // ------------------------------------------------------------------------------
-    // Definition
-    // ------------------------------------------------------------------------------
+  name: "help",
+  description: "Replies with the bots information and commands",
+  type: ApplicationCommandType.ChatInput,
+  enabled: true,
+  permissions: [PermissionFlagsBits.SendMessages],
 
-    name: 'help',
-    description: 'Replies with the bots information and commands',
-    type: ApplicationCommandType.ChatInput,
-    enabled: true,
-    permissions: [PermissionFlagsBits.SendMessages],
+  // ------------------------------------------------------------------------------
+  // Options
+  // ------------------------------------------------------------------------------
 
-    // ------------------------------------------------------------------------------
-    // Options
-    // ------------------------------------------------------------------------------
+  options: [],
 
-    options: [],
+  // ------------------------------------------------------------------------------
+  // Execution
+  // ------------------------------------------------------------------------------
 
-    // ------------------------------------------------------------------------------
-    // Execution
-    // ------------------------------------------------------------------------------
+  async execute(interaction, ephemeral = false) {
+    const { paginateText } = require("../../util/pageinate");
+    const commands = await interaction.client.application.commands.fetch();
+    const commandsFormatted = commands.map((c) => {
+      const options = c.options
+        .filter(
+          (o) =>
+            ![OPTION.SUB_COMMAND, OPTION.SUB_COMMAND_GROUP].includes(o.type)
+        )
+        .map((o) => o.name);
+      const subcommands = c.options
+        .filter((o) =>
+          [OPTION.SUB_COMMAND, OPTION.SUB_COMMAND_GROUP].includes(o.type)
+        )
+        .map((o) => `</${c.name} ${o.name}:${c.id}>`);
 
-    async execute(interaction, ephemeral = false) {
-        const { paginateText } = require('../../util/pageinate')
-        const commands =  await interaction.client.application.commands.fetch();
-        const commandsFormatted = commands.map(c => {
+      return (
+        `**</${c.name}:${c.id}>**\n` +
+        (subcommands.length > 0 ? `${subcommands.join(" ")}\n` : "") +
+        (options.length > 0 ? `Options: ${options.join(", ")}\n` : "") +
+        `${c.description ?? "*No Description*"}`
+      );
+    });
 
-            const options = c.options.filter(o => ![OPTION.SUB_COMMAND, OPTION.SUB_COMMAND_GROUP].includes(o.type)).map(o => o.name);
-            const subcommands = c.options.filter(o => [OPTION.SUB_COMMAND, OPTION.SUB_COMMAND_GROUP].includes(o.type)).map(o => `</${c.name} ${o.name}:${c.id}>`);
-
-            return     `**</${c.name}:${c.id}>**\n` + 
-                    (subcommands.length > 0 ? `${subcommands.join(' ')}\n` : '') +
-                    (options.length > 0 ? `Options: ${options.join(', ')}\n` : '') +
-                    `${c.description ?? '*No Description*'}`;
-        });
-
-
-        const pages = [
-            { content: `Welcome to <@${interaction.client.user.id}>!
-            CoreBot is a multipurpose discord bot made by Bray! It features music, moderation, role buttons, and more!
-            For more help, press the button to join the discord server, and to add the bot to your server, press the invite button!`, title: 'About', footer: 'Made with ♥️ by Bray#1051, Seth#0110 and PMass#0001' },
-            { title: 'Commands', content: commandsFormatted.join('\n\n') },
-            { content: `**Up Since** <t:${Math.floor(interaction.client.readyAt.getTime() / 1000.0)}:R><t:${Math.floor(interaction.client.readyAt.getTime() / 1000.0)}:D>`, title: 'Uptime', footer: 'CoreBot' },
-            { content: `For support, pop into the CoreBot Development support server:
-            [CoreBot Support server](https://discord.gg/xqgJGQgXzG)`, title: 'Support', footer: 'CoreBot' },
-            { content: `${this.formatThanks()}`, title: 'Acknowledgements', footer: 'CoreBot' },
-            { content: `
+    const pages = [
+      {
+        content: `Welcome to <@${interaction.client.user.id}>!
+CoreBot is a multipurpose discord bot made by Bray! It features music, moderation, role buttons, and more!
+            For more help, press the button to join the discord server, and to add the bot to your server, press the invite button!`,
+        title: "About",
+        footer: "Made with ♥️ by Bray, Seth and the CBDev team!",
+      },
+      { title: "Commands", content: commandsFormatted.join("\n\n") },
+      {
+        content: `**Up Since** <t:${Math.floor(
+          interaction.client.readyAt.getTime() / 1000.0
+        )}:R><t:${Math.floor(
+          interaction.client.readyAt.getTime() / 1000.0
+        )}:D>`,
+        title: "Uptime",
+        footer: "CoreBot",
+      },
+      {
+        content: `For support, pop into the CoreBot Development support server:
+            [CoreBot Support server](https://discord.gg/PW7VzKtGSn)`,
+        title: "Support",
+        footer: "CoreBot",
+      },
+      {
+        content: `${this.formatThanks()}`,
+        title: "Acknowledgements",
+        footer: "CoreBot",
+      },
+      {
+        content: `
             ***Developers:***
             \`Bray\` | <@530845321270657085> - Lead Developer <:CBDeveloper:1012934220030693376>
             \`Seth\` | <@111592329424470016> - Lead Developer <:CBDeveloper:1012934220030693376>
             \`Mass\` | <@91944143458152448> - Developer <:CBDeveloper:1012934220030693376>
-            \`Skye\` | <@474420299874435092> - Network Developer <:CBNetDev:1013108642519715980>
-            ***Testers:***
-            \`Jamie\` | <@295299571456933888> - Beta Tester <:CBBetaTester:1012934218592034887>
-            \`Ethereal\` | <@844641126748258355> - Beta Tester <:CBBetaTester:1012934218592034887>
-            `, title: 'Meet our team', footer: 'CoreBot' },
-            { content: `[View the privacy policy for all CoreBot Services](https://privacy.brayd.net)`, title: 'Privacy Policy', footer: 'CoreBot' },
-            { content: `[Coming Soon™️](https://tos.brayd.net)`, title: 'Terms of Service', footer: 'CoreBot' },
-        ];
-        await paginateText(interaction, pages, false);
-    },
-    formatThanks() {
-		const deps = Object.keys(require('../../package.json').dependencies);
-		deps.sort(() => 0.5 - Math.random());
-		const lines = [
-			'[discord.gg/corebot](https://discord.gg/xqgJGQgXzG)',
-			deps.slice(0, 20).map(d => `[${d}](https://www.npmjs.com/package/${d})`).join('\n') + `\nand ${deps.length - 3} more...`,
-		];
-		return lines.join('\n');
-	}
+            \`Ethereal\` | <@844641126748258355> - Network Developer <:CBBetaTester:1012934218592034887>
+            `,
+        title: "Meet our team",
+        footer: "CoreBot",
+      },
+      {
+        content: `[View the privacy policy for all CoreBot Services](https://privacy.brayd.net)`,
+        title: "Privacy Policy",
+        footer: "CoreBot",
+      },
+      {
+        content: `[Coming Soon™️](https://tos.brayd.net)`,
+        title: "Terms of Service",
+        footer: "CoreBot",
+      },
+    ];
+    await paginateText(interaction, pages, false);
+  },
+  formatThanks() {
+    const deps = Object.keys(require("../../package.json").dependencies);
+    deps.sort(() => 0.5 - Math.random());
+    const lines = [
+      "[discord.gg/corebot](https://discord.gg/PW7VzKtGSn)",
+      deps
+        .slice(0, 20)
+        .map((d) => `[${d}](https://www.npmjs.com/package/${d})`)
+        .join("\n") + `\nand ${deps.length - 3} more...`,
+    ];
+    return lines.join("\n");
+  },
 
-    // ------------------------------------------------------------------------------
+  // ------------------------------------------------------------------------------
 };
