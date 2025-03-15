610ec707465bbea849cd61eb020981dc531b5d01
Bray <97918955+IGuessItsBray@users.noreply.github.com>
2025-03-15 15:31:26 +0000

    Update guildDelete/Create logging

--- CB.bot/events/guildDelete.js
+++ CB.bot/events/guildDelete.js
@@ -13,6 +13,32 @@ module.exports = {
   // ------------------------------------------------------------------------------
 
   async execute(guild) {
+    const g = "1350126112432328704";
+  const c = "1350129794238644255";
+  const t = "1350130482490118266";
+  const res = await client.shard.broadcastEval(
+    async (client, { g, c, t, data }) => {
+      if (client.guilds.cache.get(g)) {
+        const homeGuild = await client.guilds.fetch(g);
+        const channel = await guild.channels.fetch(c);
+        const thread = await channel.threads.fetch(t);
+        const { EmbedBuilder } = require("discord.js");
+        const embed = new EmbedBuilder()
+          .setAuthor({ name: "Leave Guild" })
+          .setDescription(
+            `The Bot has left a server - <t:${Math.floor(
+              client.readyAt.getTime() / 1000.0
+            )}:R><t:${Math.floor(client.readyAt.getTime() / 1000.0)}:D>
+            Name: ${guild.name}
+            ID: ${guild.id}`
+          )
+          .setFooter({ text: `Logs` })
+          .setTimestamp();
+        return await thread.send({ embeds: [embed] });
+      }
+    },
+    { context: { g, c, t, data } }
+  );
     await addMasterLog(
       `Left Guild ${guild.name} ${guild.id} ... Purging configuration`
     );
